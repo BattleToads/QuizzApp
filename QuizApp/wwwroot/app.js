6414319375d7b9df62b575825850c420f0506aaf
Randy E <randy@battletoads.io>
2017-03-23 16:15:58 -0500

    refactored to alerts/prompts instead of HTML for now.

--- QuizApp/wwwroot/app.js
+++ QuizApp/wwwroot/app.js
@@ -9,20 +9,47 @@ var QuizApp;
             this.correctAnswer = correctAnswer;
             this.points = points;
         }
-        Question.prototype.prepHTML = function () {
-            var preppedHTML = "";
-            preppedHTML += "<h1>Q" + this.questionNumber + ": " + this.question + "</h1><hr>";
+        Question.prototype.makeString = function () {
+            var myString = "";
+            myString += this.questionNumber + ": " + this.question;
             for (var i = 0; i < this.answers.length; i++) {
-                preppedHTML += "<p><input type='radio' name='" + this.questionNumber + "' label='" + this.answers[i] + "'";
-                console.log(preppedHTML);
+                myString += "\n" + String.fromCharCode(65 + i) + ": " + this.answers[i];
             }
-            return preppedHTML;
+            return myString;
+        };
+        Question.prototype.evaluate = function (guess) {
+            if (guess.toLowerCase() == this.correctAnswer.toLowerCase()) {
+                return this.points;
+            }
+            else {
+                return 0;
+            }
         };
         return Question;
     }());
-    var questions = [
-        new Question("Is Climate Change real?", "1", ["Yes", "No", "What is Climate Change?"], "Yes", "2")
+    var Quiz = (function () {
+        function Quiz(quizQuestions) {
+            this.quizQuestions = quizQuestions;
+        }
+        Quiz.prototype.cycleQuestions = function () {
+            for (var i = 0; i < this.quizQuestions.length; i++) {
+                var guess = prompt();
+            }
+        };
+        return Quiz;
+    }());
+    var quizQuestions = [
+        new Question("Is Climate Change real?", "1", ["Yes", "No", "What is Climate Change?"], "Yes", 2),
+        new Question("What number is bigger?", "2", ["1", "2", "3"], "3", 2)
     ];
-    document.getElementById("questions").innerHTML = questions[0].prepHTML();
+    // let quiz = new Quiz(quizQuestions);
+    var totalPoints = 0;
+    var possiblePoints = 0;
+    for (var i = 0; i < quizQuestions.length; i++) {
+        var guess = prompt(quizQuestions[i].makeString());
+        totalPoints += quizQuestions[i].evaluate(guess);
+        possiblePoints += quizQuestions[i].points;
+    }
+    alert("You received " + totalPoints + " out of a possible " + possiblePoints);
 })(QuizApp || (QuizApp = {}));
 //# sourceMappingURL=app.js.map
--- QuizApp/wwwroot/app.ts
+++ QuizApp/wwwroot/app.ts
@@ -1,38 +1,78 @@
-﻿
-
-namespace QuizApp {
+﻿namespace QuizApp {
     console.log("Started");
+
     class Question {
         constructor(
             public question: string,
-            public questionNumber: string,
+            public questionNumber: any,
             public answers: string[],
             public correctAnswer: string,
-            public points: string,
+            public points: number
         ) {
-            
+
         }
 
-        public prepHTML() {
-            let preppedHTML = "";
-            preppedHTML += `<h1>Q${this.questionNumber}: ${this.question}</h1><hr>`;
+        public makeString() {
+            let myString = "";
+            myString += `${this.questionNumber}: ${this.question}`;
             for (let i = 0; i < this.answers.length; i++) {
-                preppedHTML += `<p><input type='radio' name='${this.questionNumber}' label='${this.answers[i]}'`;
-                console.log(preppedHTML);
+                myString += `\n${String.fromCharCode(65 + i)}: ${this.answers[i]}`;
             }
-            return preppedHTML;
+            return myString;
         }
+
+        public evaluate(guess: string) {
+            if (guess.toLowerCase() == this.correctAnswer.toLowerCase()) {
+                return this.points;
+            }
+            else {
+                return 0;
+            }
+        }
+
+
 
 
     }
 
-    let questions = [
-        new Question("Is Climate Change real?", "1", ["Yes", "No", "What is Climate Change?"], "Yes", "2")
+
+
+    class Quiz {
+        constructor(
+            public quizQuestions: Question[]
+        ) {
+
+
+        }
+
+
+        public cycleQuestions() {
+            for (let i = 0; i < this.quizQuestions.length; i++) {
+                let guess = prompt();
+            }
+
+
+
+        }
+    }
+    let quizQuestions = [
+        new Question("Is Climate Change real?", "1", ["Yes", "No", "What is Climate Change?"], "Yes", 2),
+        new Question("What number is bigger?", "2", ["1", "2", "3"], "3", 2)
 
     ];
 
-    document.getElementById("questions").innerHTML = questions[0].prepHTML();
-    
+    // let quiz = new Quiz(quizQuestions);
+    let totalPoints = 0;
+    let possiblePoints = 0;
 
-    
+    for (let i = 0; i < quizQuestions.length; i++) {
+        let guess = prompt(quizQuestions[i].makeString());
+        totalPoints += quizQuestions[i].evaluate(guess);
+        possiblePoints += quizQuestions[i].points;
+        
+    }
+    alert(`You received ${totalPoints} out of a possible ${possiblePoints}`);
+
+
+
 }
